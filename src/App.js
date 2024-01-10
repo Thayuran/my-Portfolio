a8aedd5405e71c52ab1f3483dccb12f02e71ddb4
Thayuran <thayuthayuran@gmail.com>
2024-01-10 19:38:29 +0530

    feat: my portfolio is added

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
       <section>
         <div>
             <h1>Welcome to Thayu Portfolio</h1>
-            <h3>Building Climber and Train Stopper</h3>
+            <h3>Make The World A Better Place With Developer.</h3>
             <p>You might saw me jumping, climbing buildings, and stopping trains. But nobody pays me a dime for that work. That's why I am learning and mastering web development. I will not stop until I become the Web Development Hero.</p>
             <a class="btn" href="#">Hire Me</a>
         </div>
