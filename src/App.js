51e859bb11796f9298e769e8edb0dd1b6adb1803
Thayuran <thayuthayuran@gmail.com>
2024-01-10 19:20:33 +0530

    feat: my exper is added

--- src/App.js
+++ src/App.js
@@ -18,7 +18,38 @@ function App() {
           Learn React
         </a>
       </header> */}
-      <p>Hi,my name is Thayuran</p>
+      {/* <p>Hi,my name is Thayuran</p> */}
+      <section>
+        <div>
+            <h1>Welcome to Thayu Portfolio</h1>
+            <h3>Building Climber and Train Stopper</h3>
+            <p>You might saw me jumping, climbing buildings, and stopping trains. But nobody pays me a dime for that work. That's why I am learning and mastering web development. I will not stop until I become the Web Development Hero.</p>
+            <a class="btn" href="#">Hire Me</a>
+        </div>
+        
+    </section>
+    
+      <div>
+            <h1>BIG DREAM</h1>
+            <h3>Become a Web Developer</h3>
+            <p>I already learned the basic HTML and CSS.My goal is to build 3 websites and learn advanced topics.</p>
+            <a class="btn" href="#">Download CV</a>
+      </div>
+   
+   
+    
+        <h1>Experience</h1>
+        <div>
+            <h2>Full Stack Web Developer</h2>
+            <h3>2022 Dec - 2023 Mar | C# Developer</h3>
+            <p>I know everything needed to make a website function, efficient. I didn't stop with the web. I went beyond with most popular Back-end framework called Laravel.I even know the deployment, server and security. I will give you 100% web solution.</p>
+        </div>
+    
+    
+    <footer>
+        <p><small>© Thayu 2024, Full Stack Developer</small></p>
+    </footer>
+
     </div>
   );
 }
